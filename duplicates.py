cd15409eece8fa43346cda2ed52189183fd60d8c
pezho <pezho.i@mail.ru>
2017-01-16 16:02:14 +0200

    first solution

--- duplicates.py
+++ duplicates.py
@@ -1,8 +1,26 @@
 
+from collections import defaultdict
+import os
 
-def are_files_duplicates(file_path1, file_path_2):
-    pass
+
+def get_all_files(path):
+    all_files = defaultdict(list)
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            full_path = os.path.join(root, file)
+            all_files[(os.path.getsize(full_path), file)].append(full_path)
+    return all_files
+
+
+def main():
+    path = input('Enter directory (.): ') or '.'
+    print('Duplicates:')
+    for key, value in get_all_files(path).items():
+        if len(value) > 1:
+            print('Find {} duplicates of file {}'.format(
+                    len(value), os.path.basename(key[1])), 
+                    '\n\t'.join(value), sep='\n\t')
 
 
 if __name__ == '__main__':
-    pass
+    main()
